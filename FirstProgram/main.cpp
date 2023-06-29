8580c45b425ace261629df5b9cd1af3763a771d7
Akash <akashkumar59999@gmail.com>
2023-06-29 08:35:31 +0530

    Finally created a repo

---
+++ FirstProgram/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream> //Because we need input/output files
+
+using namespace::std;
+
+int main() {
+    int favouriteNumber;
+    cout<<"Enter your favourite number between 1 and 100: ";
+    cin>>favouriteNumber;
+    cout<<"Your favourite number is : "<<favouriteNumber;
+    return 0;
+}
---
+++ section4Challange/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int fav_num;
+    cout<<"Enter your favourite number between 1 to 100: ";
+    cin>>fav_num;
+    cout<<"Amazing!!, that's my favourite number too!"<<endl;
+    cout<<"No Really!, "<<fav_num<<" is my favourite number";
+    return 0;
+}
